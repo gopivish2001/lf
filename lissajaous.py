bf9831c3e1d9577686be764f7f92ab25a0371abe
gopivish2001 <159097586+gopivish2001@users.noreply.github.com>
2024-02-24 16:52:08 +0530

    Update lissajaous.py

--- lissajaous.py
+++ lissajaous.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 # Set page config to wide mode
 st.set_page_config(layout="wide")
 st.title("LISSAJOUS FIGURE:-")
-text="any of an infinite variety of curves formed by combining two mutually perpendicular simple harmonic motions,\n commonly exhibited by the oscilloscope, and used in studying frequency, amplitude, and phase relations of harmonic variables."
+text="DEFINITION:- Any of an infinite variety of curves formed by combining two mutually perpendicular simple harmonic motions,\ncommonly exhibited by the oscilloscope, and used in studying frequency, amplitude, and phase relations of harmonic variables."
 st.text(text)
+st.subheader("What is the principle of Lissajous pattern?")
+st.text("The Lissajous pattern indicates the phase difference by the shape of the X-Y plot. A straight line indicates a 0º or 180º phase difference.\n The angle of the line depends on the difference in amplitude between the two signals, a line at 45º to the horizontal means the amplitudes are equal.")
 def lissajous(t, A, B, a, b, delta):
     x = A * np.sin(a * t)
     y = B * np.sin(b * t + delta)
