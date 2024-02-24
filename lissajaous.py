036e8f2857669b58c8962973418c20fb01eec07c
gopivish2001 <159097586+gopivish2001@users.noreply.github.com>
2024-02-24 16:34:34 +0530

    Update lissajaous.py

--- lissajaous.py
+++ lissajaous.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 # Set page config to wide mode
 st.set_page_config(layout="wide")
 st.title("LISSAJOUS FIGURE:-")
-st.text('any of an infinite variety of curves formed by combining two mutually perpendicular simple harmonic motions, 
-        \ncommonly exhibited by the oscilloscope, and used in studying frequency, amplitude, and phase relations of harmonic variables.')
+text='any of an infinite variety of curves formed by combining two mutually perpendicular simple harmonic motions, 
+commonly exhibited by the oscilloscope, and used in studying frequency, amplitude, and phase relations of harmonic variables.'
+st.text(text)
 def lissajous(t, A, B, a, b, delta):
     x = A * np.sin(a * t)
     y = B * np.sin(b * t + delta)
