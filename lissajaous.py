6d3cfc5d2b18aa744d1055aaab08667789f163ae
gopivish2001 <159097586+gopivish2001@users.noreply.github.com>
2024-02-24 17:50:33 +0530

    Update lissajaous.py

--- lissajaous.py
+++ lissajaous.py
@@ -9,12 +9,14 @@ multi ='''**DEFINITION:-** Any of an infinite variety of curves formed by combin
 commonly exhibited by the oscilloscope, and used in studying frequency, amplitude, and phase relations of harmonic variables.'''
 st.markdown(multi)
 st.divider()
+
 st.subheader("What is the principle of Lissajous pattern?")
 multi ='''The Lissajous pattern indicates the phase difference by the shape of the X-Y plot.
 A straight line indicates a 0º or 180º phase difference.The angle of the line depends on the 
 difference in amplitude between the two signals, a line at 45º to the horizontal means the amplitudes are equal.'''
 st.markdown(multi)
 st.divider()
+
 def lissajous(t, A, B, a, b, delta):
     x = A * np.sin(a * t)
     y = B * np.sin(b * t + delta)
@@ -24,7 +26,6 @@ col1, col2 = st.columns(2)
 def draw_lissajous(A, B, a, b, delta):
 
     with col1:
-
         t = np.linspace(0, 2 * np.pi, 1000)
         x, y = lissajous(t, A, B, a, b, delta)
         plt.figure(figsize=(10, 10))
@@ -39,8 +40,6 @@ def draw_lissajous(A, B, a, b, delta):
         st.pyplot()
 
     with col2:
-
-       
         plt.figure(figsize=(4, 2))
         plt.plot(t, x, 'r-', linewidth=2)
         plt.xlabel('Time', fontsize=14)
@@ -59,13 +58,12 @@ def draw_lissajous(A, B, a, b, delta):
         st.pyplot()
 
 st.subheader("Application of lissajous figure?")
-multi='''e Lissajou figures are used to determine the unknown frequency by comparing it with the known frequency. 
-Verifying audio oscillator with a known-frequency signal. Monitoring audio amplifiers and feedback networks for phase shift'''
+multi='''The Lissajou figures are used to determine the unknown frequency by comparing it with the known frequency. 
+Verifying audio oscillator with a known-frequency signal. Monitoring audio amplifiers and feedback networks for phase shift.'''
 st.markdown(multi)
-def main():
 
+def main():
     st.markdown("---")
-    
     st.sidebar.write("## Enter Parameters:")
     A = st.sidebar.number_input("AMPLITUDE OF X WAVE", min_value=50, value=200)
     B = st.sidebar.number_input("AMPLITUDE OF Y WAVE", min_value=50, value=300)
